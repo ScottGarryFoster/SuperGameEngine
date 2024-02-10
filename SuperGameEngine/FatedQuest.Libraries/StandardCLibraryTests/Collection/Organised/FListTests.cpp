7b0b9f05e011ece558b26d2abace9f4241a85c5b
Scott Foster <scottgarryfoster@googlemail.com>
2024-02-10 13:27:52 +0000

    Added Operator[] to FList

--- SuperGameEngine/FatedQuest.Libraries/StandardCLibraryTests/Collection/Organised/FListTests.cpp
+++ SuperGameEngine/FatedQuest.Libraries/StandardCLibraryTests/Collection/Organised/FListTests.cpp
@@ -1,6 +1,7 @@
 #include "../../LibraryIncludes.h"
 #include "../../../StandardCLibrary/Collection/Organised/FList.hpp"
 #include "FCollectionSharedTestHelpers.hpp"
+#include <string>
 
 using namespace StandardCLibraryTests_Collection_Organised;
 using namespace StandardCLibrary;
@@ -47,6 +48,52 @@ namespace StandardCLibraryTests_Collection_Organised_ListTests
         delete given;
         EXPECT_EQ(1, count);
         EXPECT_EQ(givenDerefered, actual);
+    }
+#pragma endregion
+
+#pragma region Operator[]
+    TEST(FListTests, OperatorBrackets_ReturnsElementsInOrderGiven_WhenGivenThreeElementsInOrder)
+    {
+        std::vector<int> given = { 42, 56, 99 };
+        FList<int> collection = FList<int>(given);
+
+        int actualZero = collection[0];
+        int actualOne = collection[1];
+        int actualTwo = collection[2];
+
+        EXPECT_EQ(given[0], actualZero);
+        EXPECT_EQ(given[1], actualOne);
+        EXPECT_EQ(given[2], actualTwo);
+    }
+
+    TEST(FListTests, OperatorBrackets_ReturnsElementsInOrderGiven_WhenGivenThreeElementsInOrderWithConst)
+    {
+        std::vector<int> given = { 42, 56, 99 };
+        const FList<int> collection = FList<int>(given);
+
+        int actualZero = collection[0];
+        int actualOne = collection[1];
+        int actualTwo = collection[2];
+
+        EXPECT_EQ(given[0], actualZero);
+        EXPECT_EQ(given[1], actualOne);
+        EXPECT_EQ(given[2], actualTwo);
+    }
+
+    TEST(FListTests, OperatorBrackets_ReturnsElementsInOrderGivenAsPointers_WhenGivenThreeElementsInOrder)
+    {
+        std::vector<int*> given = { new int(42), new int (56), new int(99) };
+        std::vector<int> givenDeref = { 42, 56, 99 };
+        FList<int*> collection = FList<int*>(given);
+
+        int actualZero = *collection[0];
+        int actualOne = *collection[1];
+        int actualTwo = *collection[2];
+
+        CleanUp(given);
+        EXPECT_EQ(givenDeref[0], actualZero);
+        EXPECT_EQ(givenDeref[1], actualOne);
+        EXPECT_EQ(givenDeref[2], actualTwo);
     }
 #pragma endregion
 }
