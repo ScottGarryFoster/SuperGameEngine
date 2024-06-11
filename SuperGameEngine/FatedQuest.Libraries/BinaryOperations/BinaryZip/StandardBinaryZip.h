0bd7b6eb66b17a167b99a44a997b2c28684b4566
Scott Foster <scottgarryfoster@googlemail.com>
2024-06-11 17:24:59 +0100

    Correct comments.

--- SuperGameEngine/FatedQuest.Libraries/BinaryOperations/BinaryZip/StandardBinaryZip.h
+++ SuperGameEngine/FatedQuest.Libraries/BinaryOperations/BinaryZip/StandardBinaryZip.h
@@ -17,8 +17,8 @@ namespace BinaryOperations
         /// <summary>
         /// Takes a single file and makes it binary.
         /// </summary>
-        /// <param name="input">Filepath in a Raw non-binary form. </param>
-        /// <param name="output">The Filepath to the store the binary file. </param>
+        /// <param name="inputFilepath">Filepath in a Raw non-binary form. </param>
+        /// <param name="outputFilepath">The Filepath to the store the binary file. </param>
         /// <param name="errors">If not created successfully this contains the errors. </param>
         /// <returns>True means created. </returns>
         virtual bool FileToBinary(const std::string& inputFilepath, const std::string& outputFilepath, std::vector<std::string>& errors);
@@ -26,8 +26,8 @@ namespace BinaryOperations
         /// <summary>
         /// Takes a binary file and outputs the raw data.
         /// </summary>
-        /// <param name="input">A binary filepath. </param>
-        /// <param name="output">The destination file path.</param>
+        /// <param name="inputFilepath">A binary filepath. </param>
+        /// <param name="outputFilepath">The destination file path.</param>
         /// <param name="errors">If not created successfully this contains the errors. </param>
         /// <returns>True means created.</returns>
         virtual bool BinaryToFile(const std::string& inputFilepath, const std::string& outputFilepath, std::vector<std::string>& errors);
@@ -35,8 +35,8 @@ namespace BinaryOperations
         /// <summary>
         /// Takes all the files in a directory and makes them binary.
         /// </summary>
-        /// <param name="input">A directory path with files. </param>
-        /// <param name="output">Where to store the directory with Binary Files as a path. </param>
+        /// <param name="inputPath">A directory path with files. </param>
+        /// <param name="outputPath">Where to store the directory with Binary Files as a path. </param>
         /// <param name="errors">If not created successfully this contains the errors. </param>
         /// <returns>True means created successfully. </returns>
         virtual bool DirectoryToBinary(const std::string& inputPath, const std::string& outputPath, std::vector<std::string>& errors);
@@ -44,8 +44,8 @@ namespace BinaryOperations
         /// <summary>
         /// Takes a directory with Binary files and provides the raw data.
         /// </summary>
-        /// <param name="input">A directory path with Binary data. </param>
-        /// <param name="output">A directory path to store the raw files. </param>
+        /// <param name="inputPath">A directory path with Binary data. </param>
+        /// <param name="outputPath">A directory path to store the raw files. </param>
         /// <param name="errors">If not created successfully this contains the errors. </param>
         /// <returns>True means created succesfully. </returns>
         virtual bool BinaryDirectoryToDirectory(const std::string& inputPath, const std::string& outputPath, std::vector<std::string>& errors);
@@ -53,8 +53,8 @@ namespace BinaryOperations
         /// <summary>
         /// Takes a directory of files and stores them in a Zip.
         /// </summary>
-        /// <param name="input">Directory path to store in zip. </param>
-        /// <param name="output">A location and filename for the zip. </param>
+        /// <param name="inputPath">Directory path to store in zip. </param>
+        /// <param name="outputFilepath">A location and filename for the zip. </param>
         /// <param name="errors">If not created successfully this contains the errors. </param>
         /// <returns>True means created sucessfully. </returns>
         virtual bool DirectoryToZip(const std::string& inputPath, const std::string& outputFilepath, std::vector<std::string>& errors);
@@ -62,8 +62,8 @@ namespace BinaryOperations
         /// <summary>
         /// Takes a zip and outputs a directory containing all the contents of the zip.
         /// </summary>
-        /// <param name="input">A zip file. </param>
-        /// <param name="output">A location to store the contents of the zip. </param>
+        /// <param name="inputFilepath">A zip file. </param>
+        /// <param name="outputPath">A location to store the contents of the zip. </param>
         /// <param name="errors">If not created successfully this contains the errors. </param>
         /// <returns>True means created successfully. </returns>
         virtual bool ZipToDirectory(const std::string& inputFilepath, const std::string& outputPath, std::vector<std::string>& errors);
@@ -71,8 +71,8 @@ namespace BinaryOperations
         /// <summary>
         /// Takes a directory of files, converts them to binary and then adds them to a zip file.
         /// </summary>
-        /// <param name="input">Directory of files. </param>
-        /// <param name="output">A location and filename for a zip file. </param>
+        /// <param name="inputPath">Directory of files. </param>
+        /// <param name="outputFilepath">A location and filename for a zip file. </param>
         /// <param name="errors">If not created successfully this contains the errors. </param>
         /// <returns>True means created successfully. </returns>
         virtual bool DirectoryToZipWithFileToBinary(const std::string& inputPath, const std::string& outputFilepath, std::vector<std::string>& errors);
@@ -91,8 +91,8 @@ namespace BinaryOperations
         /// Reads file contents into Stream.
         /// </summary>
         /// <param name="inputFilepath">File path.</param>
-        /// <param name="errors"></param>
-        /// <returns></returns>
+        /// <param name="errors">Errors if any. </param>
+        /// <returns>Stream of data. </returns>
         std::ifstream ReadFileContentsToBinaryStream(const std::string& inputFilepath, std::vector<std::string>& errors);
 
         /// <summary>
