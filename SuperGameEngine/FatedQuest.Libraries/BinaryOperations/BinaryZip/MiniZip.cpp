a80d8069c128151efae144d041091f674048e991
Scott Foster <scottgarryfoster@googlemail.com>
2024-06-11 18:31:37 +0100

    [#75] Started to extract Zip implementation

---
+++ SuperGameEngine/FatedQuest.Libraries/BinaryOperations/BinaryZip/MiniZip.cpp
@@ -0,0 +1,6 @@
+#include "MiniZip.h"
+
+bool BinaryOperations::MiniZip::CreateZip(const std::string& filename, const std::vector<std::string>& filenames, const ZipCreateOpenOption& options)
+{
+    return false;
+}
