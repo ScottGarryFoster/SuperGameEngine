2fbc2110cc6023ad52bfea6fe27088e7c9f682ca
Scott Foster <scottgarryfoster@googlemail.com>
2024-12-22 20:05:33 +0000

    Fixing a reference issue with a stub

--- FatedQuest.Libraries/GamePackageTests/GamePackage/Stub/StubBinaryZip.h
+++ FatedQuest.Libraries/GamePackageTests/GamePackage/Stub/StubBinaryZip.h
@@ -15,64 +15,64 @@ namespace FatedQuestLibraries_GamePackage
     {
     public:
         
-        virtual bool FileToBinary(
+        inline virtual bool FileToBinary(
             const std::string& inputFilepath,
             const std::string& outputFilepath,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual bool BinaryToFile(
+        inline virtual bool BinaryToFile(
             const std::string& inputFilepath,
             const std::string& outputFilepath,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual bool BinaryToData(
+        inline virtual bool BinaryToData(
             const std::string& inputFilepath,
             std::vector<unsigned char>& data,
             std::vector<std::string>& errors) const override  { return false; }
 
-        virtual bool DirectoryToBinary(
+        inline virtual bool DirectoryToBinary(
             const std::string& inputPath,
             const std::string& outputPath,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual bool BinaryDirectoryToDirectory(
+        inline virtual bool BinaryDirectoryToDirectory(
             const std::string& inputPath,
             const std::string& outputPath,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual bool DirectoryToZip(
-            const std::string& inputPath,
-            const std::string& outputFilepath,
-            std::vector<std::string>& errors) const override { return false; }
-
-        virtual bool ZipToDirectory(
-            const std::string& inputFilepath,
-            const std::string& outputPath,
-            std::vector<std::string>& errors) const override { return false; }
-
-        virtual bool DirectoryToZipWithFileToBinary(
+        inline virtual bool DirectoryToZip(
             const std::string& inputPath,
             const std::string& outputFilepath,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual bool ZipWithBinaryToDirectoryToFile(
+        inline virtual bool ZipToDirectory(
             const std::string& inputFilepath,
             const std::string& outputPath,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual bool ExtractSingleFileToData(
+        inline virtual bool DirectoryToZipWithFileToBinary(
+            const std::string& inputPath,
+            const std::string& outputFilepath,
+            std::vector<std::string>& errors) const override { return false; }
+
+        inline virtual bool ZipWithBinaryToDirectoryToFile(
+            const std::string& inputFilepath,
+            const std::string& outputPath,
+            std::vector<std::string>& errors) const override { return false; }
+
+        inline virtual bool ExtractSingleFileToData(
             const std::string& zipFilePath,
             const std::string& innerFilepath,
             std::vector<unsigned char>& data,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual bool ExtractSingleBinaryFileToData(
+        inline virtual bool ExtractSingleBinaryFileToData(
             const std::string& zipFilePath,
             const std::string& innerFilepath,
             std::vector<unsigned char>& data,
             std::vector<std::string>& errors) const override { return false; }
 
-        virtual std::vector<std::string> ListFilesInArchive(
+        inline virtual std::vector<std::string> ListFilesInArchive(
             const std::string& filepath,
             std::vector<std::string>& errors) const override { return {}; }
     };
--- FatedQuest.Libraries/GamePackageTests/GamePackage/Stub/StubPackageFile.h
+++ FatedQuest.Libraries/GamePackageTests/GamePackage/Stub/StubPackageFile.h
@@ -18,28 +18,28 @@ namespace FatedQuestLibraries_GamePackage
         /// Returns file path.
         /// </summary>
         /// <returns>File path of the file. </returns>
-        virtual std::string Path() const { return m_path; }
+        virtual std::string Path() const override { return m_path; }
         std::string m_path = {};
 
         /// <summary>
         /// File path with all directories split into elements of a vector.
         /// </summary>
         /// <returns>File path with all directories split into elements of a vector. </returns>
-        virtual std::vector<std::string> PathByDirectory() const { return m_pathByDirectory; }
+        virtual std::vector<std::string> PathByDirectory() const override { return m_pathByDirectory; }
         std::vector<std::string> m_pathByDirectory = {};
 
         /// <summary>
         /// Reads file contents.
         /// </summary>
         /// <returns>File contents. </returns>
-        virtual std::string ReadFileContents() { return m_fileContents; }
+        virtual std::string ReadFileContents() override { return m_fileContents; }
         std::string m_fileContents = {};
 
         /// <summary>
         /// Reads file contents.
         /// </summary>
         /// <returns>File contents. </returns>
-        virtual std::vector<std::string> ReadFileContentsByLine() { return m_fileContentsByLine; }
+        virtual std::vector<std::string> ReadFileContentsByLine() override { return m_fileContentsByLine; }
         std::vector<std::string> m_fileContentsByLine = {};
 
         /// <summary>
@@ -47,14 +47,14 @@ namespace FatedQuestLibraries_GamePackage
         /// </summary>
         /// <param name="extension">Extension to look for. </param>
         /// <returns>True means has extension. </returns>
-        virtual bool EndInExtension(const std::string& extension) const { return m_endInExtension; }
+        virtual bool EndInExtension(const std::string& extension) const override { return m_endInExtension; }
         bool m_endInExtension = false;
 
         /// <summary>
         /// Gets the filename with extension.
         /// </summary>
         /// <returns>Filename with extension. </returns>
-        virtual std::string FileName() const { return m_fileName; }
+        virtual std::string FileName() const override { return m_fileName; }
         std::string m_fileName = {};
 
         /// <summary>
@@ -62,7 +62,20 @@ namespace FatedQuestLibraries_GamePackage
         /// period counts as the extension.
         /// </summary>
         /// <returns></returns>
-        virtual std::string Extension() const { return m_fileName; }
+        virtual std::string Extension() const override { return m_fileName; }
         std::string m_extension = {};
+
+        /// <summary>
+        /// Gets the storage type as in whether the file format is stored
+        /// as binary or is untouched from its original form.
+        /// Use this to alter which method of file reading you perform.
+        /// </summary>
+        /// <returns>
+        /// The form of the file packaged. This should help to inform you
+        /// of which method to use when gathering the data back out of a package
+        /// file.
+        /// </returns>
+        virtual PackageFileStorageType GetStorageType() const override { return m_storageType; }
+        PackageFileStorageType m_storageType;
     };
 }
