92cfeb802d0fbb600bdb187add9407dfd4e47e93
Scott Foster <scottgarryfoster@googlemail.com>
2025-01-01 15:58:50 +0000

    Removed a comment from a previous implementation

--- SuperGameEngine/Tools/Windows/SceneHierarchy/SceneHierarchy.cpp
+++ SuperGameEngine/Tools/Windows/SceneHierarchy/SceneHierarchy.cpp
@@ -7,20 +7,6 @@ using namespace SuperGameTools;
 
 void SceneHierarchy::Setup(const std::shared_ptr<WindowPackage>& windowPackage)
 {
-    //TreeNode child1;
-    //child1.label = "Inner1";
-    //child1.id = 1;
-
-    //TreeNode child2;
-    //child2.label = "Inner2";
-    //child2.id = 2;
-
-    //TreeNode root;
-    //root.label = "Root";
-    //root.id = 0;
-    //root.children = { child1, child2 };
-
-    //m_treeNode = root;
     auto child1 = std::make_shared<TreeViewItem>();
     child1->GetLabel()->SetValue("Child 1");
 
